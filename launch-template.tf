cc46a156156f63fc890811e394cd18d33286070b
Ramprasad <ramprasad@Ramprasads-MacBook-Air.local>
2024-12-22 14:34:51 +0530

    4 commit

---
+++ launch-template.tf
@@ -0,0 +1,13 @@
+resource "aws_launch_template" "demo-lt" {
+    name = "aws-launch-template"
+    image_id = var.ami
+    instance_type = var.instance
+    key_name = "demo-key"
+
+    network_interfaces {
+      associate_public_ip_address = false
+      security_groups = [ aws_security_group.demo-sg.id ]
+    }
+  
+}
+
